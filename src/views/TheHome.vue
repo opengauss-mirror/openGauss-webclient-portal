3b14f68db0f0173bff7922948570530cd1069e5b
Bang <15622356989@163.com>
2022-06-08 14:19:58 +0800

    feat: add iframe

--- src/views/TheHome.vue
+++ src/views/TheHome.vue
@@ -1,12 +1,42 @@
 <script setup>
-import { ref } from 'vue';
+import { useLoginStore } from '@/stores';
+import { onMounted, ref, watch } from 'vue';
+
+const loginStore = useLoginStore();
 
 const clientSrc = ref('http://localhost:8081/');
+const iframeIns = ref(null);
+onMounted(() => {
+  const iframeWin = iframeIns.value.contentWindow;
+
+  watch(
+    () => {
+      return loginStore.isLogined;
+    },
+    (val) => {
+      if (val) {
+        iframeWin.postMessage(
+          {
+            id: 'id',
+            token: 'token',
+          },
+          clientSrc.value
+        );
+      }
+    },
+    { immediate: true }
+  );
+});
 </script>
 
 <template>
   <div class="home">
-    <iframe :src="clientSrc" width="100%" height="100%"></iframe>
+    <iframe
+      ref="iframeIns"
+      :src="clientSrc"
+      width="100%"
+      height="100%"
+    ></iframe>
   </div>
 </template>
 
@@ -14,5 +44,9 @@ const clientSrc = ref('http://localhost:8081/');
 .home {
   width: 100vw;
   height: 100vh;
+}
+
+iframe {
+  border: none;
 }
 </style>
