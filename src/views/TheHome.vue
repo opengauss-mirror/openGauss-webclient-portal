596a2c6e6054f82a01f55a2a53540766050426f3
Bang <15622356989@163.com>
2022-07-04 09:18:52 +0800

    feat: 退出登录

--- src/views/TheHome.vue
+++ src/views/TheHome.vue
@@ -1,23 +1,18 @@
 <script setup>
-import { useLoginStore, useUserInfoStore } from '@/stores';
-import { doLogin } from '@/shared/login';
+import { useUserInfoStore } from '@/stores';
+import { logout } from '@/shared/login';
 import { onMounted, onUnmounted, ref, watch } from 'vue';
 import { useRouter } from 'vue-router';
-
-const loginStore = useLoginStore();
 const userInfoStore = useUserInfoStore();
+
 const router = useRouter();
 
-if (!loginStore.isLogined) {
-  if (localStorage.getItem('_U_T_')) {
-    doLogin();
-  } else {
-    router.push('/login');
+const handleMessage = (e) => {
+  const data = e.data;
+  if (data.events === 'logout') {
+    logout();
+    router.go('/login');
   }
-}
-
-const handleMessage = (data) => {
-  console.log(data);
 };
 
 const clientSrc = ref('');
@@ -32,7 +27,7 @@ onMounted(() => {
     },
     (val) => {
       if (val) {
-        clientSrc.value = userInfoStore.subdomain;
+        clientSrc.value = 'http://localhost:8081/';
         if (iframeDom.attachEvent) {
           iframeDom.attachEvent('onload', function () {
             iframeWin.postMessage(
@@ -60,6 +55,8 @@ onMounted(() => {
     },
     { immediate: true }
   );
+
+  window.addEventListener('message', handleMessage);
 });
 
 onUnmounted(() => {
